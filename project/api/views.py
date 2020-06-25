5f186b275c651349d1dc9825236f7320e5144c27
Sajan4072 <rai4072@gmail.com>
2020-06-25 20:13:15 +0545

    api_view of article list

--- project/api/views.py
+++ project/api/views.py
@@ -25,7 +25,7 @@ def article_list(request):
             return Response(serializer.data,status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)    
 
-@csrf_exempt
+
 def article_detail(request,pk):
     try:
         article=Article.objects.get(pk=pk)
