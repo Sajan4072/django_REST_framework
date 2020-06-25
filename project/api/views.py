6330e2a4bd6574922742a8cb99917509a9dac927
Sajan4072 <rai4072@gmail.com>
2020-06-25 18:38:41 +0545

    update,delete apis

--- project/api/views.py
+++ project/api/views.py
@@ -22,3 +22,27 @@ def article_list(request):
             return JsonResponse(serializer.data,status=201)
         return JsonResponse(serializer.errors,status=400)    
 
+@csrf_exempt
+def article_detail(request,pk):
+    try:
+        article=Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method=='GET':
+        serializer=ArticleSerializer(article) 
+        return JsonResponse(serializer.data)
+    elif request.method=='PUT':
+        data=JSONParser().parse(request)
+        serializer=ArticleSerializer(article,data=data) 
+
+        if  serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data) 
+        return JsonResponse(serializer.errors,status=400)
+
+    elif request.method=='DELETE':
+        article.delete()
+        return HttpResponse(status=204)
+
+          
