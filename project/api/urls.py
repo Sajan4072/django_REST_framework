3640a31d4ece36c51ea117396a08912d073ca702
Sajan4072 <rai4072@gmail.com>
2020-06-25 18:59:54 +0545

    detail path added

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list,article_detail
 
 urlpatterns=[
     path('article/',article_list),
+    path('detail/<int:pk>',article_detail)
 ]
