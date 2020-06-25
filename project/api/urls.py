1b78f1ab66bf8eb56bda607b4e186b74c7b395c1
Sajan4072 <rai4072@gmail.com>
2020-06-25 17:11:23 +0545

    csrf exempt

---
+++ project/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import article_list
+
+urlpatterns=[
+    path('article/',article_list),
+]
--- project/api/views.py
+++ project/api/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
+@csrf_exempt
 def article_list(request):
     if request.method=='GET':
         articles=Article.objects.all()
